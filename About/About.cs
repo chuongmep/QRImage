5d470406c9eb1bf5e3d1d539098e1087adf458e2
ho chuong <chuongpqvn@gmail.com>
2020-03-16 21:52:10 +0700

    re

---
+++ About/About.cs
@@ -0,0 +1,19 @@
+﻿
+namespace QRImage.About
+{
+    /// <summary>
+    ///     Information All Package
+    /// </summary>
+    public static class Information
+    {
+        
+        /// <summary>
+        ///     Contact with Author
+        /// </summary>
+        /// <returns></returns>
+        public static string Author()
+        {
+            return "Chuongpqvn@gmail.com";
+        }
+    }
+}
---
+++ Color/Colors.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace QRImage.Color
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Colors
+    
+    {
+        private Colors() { }
+
+        /// <summary>
+        /// Select Color ByName Input String Color
+        /// </summary>
+        /// <param name="NameColor"></param>
+        /// <returns></returns>
+        public static System.Drawing.Color SelectColorByName(string NameColor)
+        {
+            System.Drawing.Color Color = System.Drawing.Color.FromName(NameColor);
+            return Color;
+        }
+
+        /// <summary>
+        /// Return All Color
+        /// </summary>
+        /// <returns name = "ListColor"></returns>
+       
+        public static IDictionary GetAllColors()
+        {
+            List<System.Drawing.Color> allColors = new List<System.Drawing.Color>();
+
+            List<string> NameColor = new List<string>();
+            foreach (PropertyInfo property in typeof(System.Drawing.Color).GetProperties())
+            {
+                if (property.PropertyType == typeof(System.Drawing.Color))
+                {
+                    allColors.Add((System.Drawing.Color)property.GetValue(null));
+                }
+            }
+
+            foreach (System.Drawing.Color c in allColors) NameColor.Add(c.Name);
+
+            return new Dictionary<string,object>{{"Colors",allColors},{"NameColor",NameColor}};
+        }
+    }
+
+}
